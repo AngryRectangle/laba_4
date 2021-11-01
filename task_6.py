069e7f62be575192ba13f45f4aad640e59d506c0
AngryRectangle <yashmel2000@gmail.com>
2021-11-01 18:59:11 +0300

    Task 6

---
+++ task_6.py
@@ -0,0 +1,48 @@
+import math
+import helper
+
+
+def radix_sort(a, result, power, l, r):
+    if power == 0:
+        return
+
+    c = [0] * 10
+    ten_powered = 10 ** power
+    ten_less_powered = 10 ** (power - 1)
+    for i in range(l, r):
+        c[(a[i] % ten_powered) // ten_less_powered] += 1
+
+    count = 0
+    for j in range(0, 10):
+        tmp = c[j]
+        c[j] = count
+        count += tmp
+
+    for j in range(l, r):
+        d = (a[j] % ten_powered) // ten_less_powered
+        result[c[d] + l] = a[j]
+        c[d] += 1
+
+    for i in range(l, r):
+        a[i] = result[i]
+
+    last = 0
+    for i in range(0, 10):
+        radix_sort(a, result, power - 1, last, c[i])
+        last = c[i]
+
+
+def sort(a, b):
+    res = [0] * len(a) * len(b)
+    for i in range(0, len(res)):
+        res[i] = a[i % len(a)] * b[i // len(a)]
+    sum = 0
+    n = [0] * len(res)
+    max_power = math.ceil(math.log10(max(res)))
+    radix_sort(res, n, max_power, 0, len(n))
+    for i in range(0, len(n), 10):
+        sum += n[i]
+    return sum
+
+
+print(sort([7, 1, 4, 9], [2, 7, 8, 11]))
