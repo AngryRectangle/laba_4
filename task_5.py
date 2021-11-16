0bcfb67182f26f589e7f2cee740081aade2cc012
AngryRectangle <yashmel2000@gmail.com>
2021-11-16 23:26:42 +0300

    Finish task 5

--- task_5.py
+++ task_5.py
@@ -1,13 +1,51 @@
-import task_1
-import helper
+class TrueStack:
+    stack = []
+    length = 0
+    capacity = 0
+
+    def add(self, elem):
+        if self.length == self.capacity:
+            self.ensureCapacity(max(1, self.capacity * 2))
+        prevMax = 0
+        if self.length > 0:
+            prevMax = max(self.stack[self.length - 1])
+        self.stack[self.length] = (elem, prevMax)
+        self.length += 1
+
+    def pop(self):
+        val = self.stack[self.length - 1]
+        self.length -= 1
+        return val[0]
+
+    def last(self):
+        return self.stack[self.length - 1]
+
+    def ensureCapacity(self, count):
+        self.stack.extend([None] * (count - self.capacity))
+        self.capacity = count
+
+    def maxFrom(self):
+        return max(self.stack[self.length - 1])
+
+    def clear(self):
+        self.length = 0
 
 
-def h_index(arr):
-    task_1.rand_quick_sort(arr, 0, len(arr) - 1)
-    m = 0
-    for i in range(len(arr) - 1, 0, -1):
-        m = max(m, min(len(arr) - i, arr[i]))
-    return m
+tStack = TrueStack()
+file = open("input.txt", "r")
+result = []
+count = int(file.readline())
 
+for line in file.readlines():
+    data = line.split()
+    if data[0] == "push":
+        tStack.add(int(data[1]))
+    elif data[0] == "max":
+        result.append(str(tStack.maxFrom()))
+    elif data[0] == "pop":
+        tStack.pop()
 
-print(h_index(helper.rand_array(5000, 0, 1000)))
+write = open("output.txt", "w")
+write.write("\n".join(result))
+file.close()
+write.close()
